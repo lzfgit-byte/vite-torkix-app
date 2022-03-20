6b5dfb0c155594484a827bfdbbb77fb223358d9f
180745 <1807409742@qq.com>
2022-03-20 23:19:59 +0800

    ci: 持续性更新

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="topLeft">topLeft</div>
+    <div class="topLeft">
+        <a>Gmail</a>
+        <a>图片</a>
+        <div class="appDot displayInline"
+            ><svg width="24" height="24" viewBox="0 0 24 24">
+                <path
+                    d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"
+                ></path></svg
+        ></div>
+        <div class="photoContain displayInline">
+            <img width="45" src="public/imgs/tx.png" />
+        </div>
+    </div>
     <div class="grid-contain">
         <div class="iconNav">
             <div class="logoContain">
