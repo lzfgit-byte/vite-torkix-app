bf64479417c45746ba760b42bf2cc6c8a2ef487f
180745 <1807409742@qq.com>
2022-03-20 20:05:17 +0800

    ci: 持续性更新

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,4 @@
 
 <script setup lang="ts">
     import Navigation from './components/navigation/navigation.vue';
-    import HelloWorld from './components/HelloWorld.vue';
 </script>
--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -1,8 +1,36 @@
 <template>
     <div class="topLeft">topLeft</div>
     <div class="grid-contain">
-        <div class="iconNav">iconNav</div>
-        <div class="inptNav">inptNav</div>
+        <div class="iconNav">
+            <div class="logoContain">
+                <div class="logo">
+                    <img width="272" height="92" src="public/imgs/google_logo.svg" />
+                </div>
+            </div>
+        </div>
+        <div class="inptNav">
+            <div class="searchInputContain">
+                <div class="search">
+                    <div class="searchIconContain">
+                        <img
+                            width="20"
+                            height="20"
+                            class="searchIcon"
+                            src="public/imgs/search.svg"
+                        />
+                    </div>
+                    <input
+                        id="input"
+                        type="search"
+                        autocomplete="off"
+                        spellcheck="false"
+                        role="combobox"
+                        aria-live="polite"
+                        placeholder="在 Google 上搜索，或者输入一个网址"
+                    />
+                </div>
+            </div>
+        </div>
         <div class="barNav">barNav</div>
     </div>
 </template>
@@ -11,6 +39,7 @@
     import { ref, reactive, PropType } from 'vue';
     import { webSiteProp, searchWebSit } from './type/navigation-types';
     import NavCard from './nav-card.vue';
+    import Index from '../../../index.html';
     defineProps({
         website: {
             type: Array as PropType<webSiteProp[]>,
