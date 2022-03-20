4ca80953976bf47f78890bb478f384a2f290f86a
180745 <1807409742@qq.com>
2022-03-20 23:34:54 +0800

    ci: 持续性更新

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="topLeft">
-        <a>Gmail</a>
-        <a>图片</a>
-        <div class="appDot displayInline"
-            ><svg width="24" height="24" viewBox="0 0 24 24">
+        <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl" target="_self">Gmail</a>
+        <a href="https://www.google.com.hk/imghp?hl=zh-CN&tab=ri&ogbl" target="_self">图片</a>
+        <div class="appDot">
+            <svg width="24" height="24" viewBox="0 0 24 24">
                 <path
                     d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"
-                ></path></svg
-        ></div>
+                ></path>
+            </svg>
+        </div>
         <div class="photoContain displayInline">
             <img width="45" src="public/imgs/tx.png" />
         </div>
